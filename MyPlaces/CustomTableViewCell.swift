bfe8bc53d155985d5a40d872bb29a2a5129bfcc5
Vitaliy Lipovetskiy <vitaliy1972li@gmail.com>
2020-06-13 22:47:47 +0300

    Custom Table View Cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Виталий Липовецкий on 13.06.2020.
+//  Copyright © 2020 Виталий Липовецкий. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+
+}
