bdbeaa4b7ac869bfdadedd53e4afc158e7feafb6
Vitaliy Lipovetskiy <vitaliy1972li@gmail.com>
2020-06-13 18:55:41 +0300

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Виталий Липовецкий on 13.06.2020.
+//  Copyright © 2020 Виталий Липовецкий. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
